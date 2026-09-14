0e89f3798e79918377d72a9c0e0d97dbd1c4a6d1
RodrigoR-1 <rodjunior875@gmail.com>
2026-09-14 11:14:56 +0000

    prova

---
+++ main.py
@@ -0,0 +1,21 @@
+#Cyber Pulse Tech
+
+startups = {
+    "nome":"Cyber Pulse Tech",
+    "segmento":"Segurança da Informação",
+    "ano_adesao":2026
+}
+soluções_ativas = ["Fire Wall IA","Scan de Vulerabilidades"]
+print("STARTUP===", startups["nome"],"===")
+print("Projeto em andamento",soluções_ativas[0])
+
+#Bancadas de Trabalho
+
+bancadas = [
+    ["Bancada N1 ocupada","Bancada N2 Livre"],
+    ["Bancada S1 Ocupada","Bancada S2 Livre"]
+]
+print("bancada N1:",bancadas[0][0])
+print("bancada N2:",bancadas[0][1])
+print("bancada S1:",bancadas[1][0])
+print("bancada S2:",bancadas[1][1])
